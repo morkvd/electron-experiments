9ffa600198e8eef01dbca2c0af2059deb637403a
morkvd <markvdijken@gmail.com>
2016-05-29 16:50:50 +0200

    ¯\_(ツ)_/¯

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            0,
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-cond-assign": [
+            "error",
+            "except-parens"
+        ],
+        "no-console": ["error", { "allow": ["log", "warn", "error"] }]
+    }
+};
---
+++ main.js
@@ -0,0 +1,105 @@
+// App controls application life, BrowserWindow creates native browser window,
+// globalShortcut registers keys as input, ipcMain handles communication from
+// window to main process.
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  ipcMain
+} = require('electron');
+
+// Global reference of the window object, to stop the window from closing
+// automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// create a window, load the index.html, on close dereference window obj.
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 720,
+    height: 752, // 720 + 32
+    frame: false,
+    resizable: false,
+    maximizable: false,
+    title: 'VIDEOJAMES'
+  });
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.on('closed', () => mainWindow = null);
+
+  // registers and unregisters keys on blur and focus so the keys don't get
+  // intercepted when the user switches to another window.
+  mainWindow.on('focus', registerKeys);
+  mainWindow.on('blur', () => {
+    globalShortcut.unregisterAll();
+  });
+}
+
+// called when Electron has finished initialization and is ready to create
+// browser windows. some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  createWindow();
+});
+// Quit when all windows are closed.
+// On OS X applications stay active until the user quits with Cmd + Q
+app.on('window-all-closed', () => {
+  globalShortcut.unregisterAll();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+// On OS X it's common to re-create a window when the dock icon is clicked
+app.on('activate', () => {
+  if (mainWindow === null) createWindow();
+});
+
+// handle menubar buttons (exit and minimize) in the window
+ipcMain.on('exit-request', () => {
+  console.log('HALTING');
+  globalShortcut.unregisterAll();
+  app.quit();
+});
+ipcMain.on('minimize-request', () => {
+  globalShortcut.unregisterAll();
+  mainWindow.minimize();
+});
+
+
+// # INPUT
+function registerKeys() {
+  const usedKeys =
+    ['Up', 'Down', 'Left', 'Right', 'CommandOrControl+Q', 'Space'];
+  usedKeys.forEach(key => {
+    globalShortcut.register(key, () => handleKey(key));
+  });
+}
+
+function handleKey(key) {
+  console.log(`${key} is used`);
+  if (key === 'CommandOrControl+Q') {
+    globalShortcut.unregisterAll();
+    app.quit();
+  } else if (key === 'Space') {
+    mainWindow.webContents.send('level-updated', generateLevel());
+  }
+}
+
+
+// # GAME LOGIC
+
+// Global reference to the current state of the game
+const gamestate = {
+  level: [],
+  playerPos: [0, 0]
+};
+
+// level generation
+function generateLevel() {
+  let lvl = [];
+  let size = 30;
+  for (let i = 0; i < size; i++) {
+    lvl.push([]);
+    for (let j = 0; j < size; j++) {
+      lvl[i].push( 'X' );
+    }
+  }
+  return lvl;
+}
